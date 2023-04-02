a30d5495c22eb815a395f00d43b4e1d72a51acdc
xMaddMackx <82854182+xMaddMackx@users.noreply.github.com>
2023-04-02 19:25:55 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+game "gta5"
+
+lua54 "yes"
+
+author "XMMX Development"
+description "Gabz MBA EntitySet Changer"
+version "1.0"
+
+client_scripts {
+    'shared.lua',
+    'client/*.lua'
+}
+
+server_scripts {
+    'shared.lua',
+    'server/*.lua'
+}
---
+++ shared.lua
@@ -0,0 +1,144 @@
+entitySet = {
+    ["BASKETBALL"] = {
+        'mba_tribune', 
+        'mba_tarps', 
+        'mba_basketball', 
+        'mba_jumbotron'
+    },
+    ["BOXING"] = {
+        'mba_tribune', 
+        'mba_tarps', 
+        'mba_fighting', 
+        'mba_boxing', 
+        'mba_jumbotron'
+    },
+    ["CONCERT"] = {
+        'mba_tribune', 
+        'mba_tarps', 
+        'mba_backstage', 
+        'mba_concert', 
+        'mba_jumbotron'
+    },
+    ["CURLING"] = {
+        'mba_tribune', 
+        'mba_chairs', 
+        'mba_curling'
+    },
+    ["DERBY"] = {
+        'mba_cover', 
+        'mba_terrain', 
+        'mba_derby', 
+        'mba_ring_of_fire'
+    },
+    ["FAMEorSHAME"] = {
+        'mba_tribune', 
+        'mba_tarps', 
+        'mba_backstage', 
+        'mba_fameorshame', 
+        'mba_jumbotron'
+    },
+    ["FASHION"] = {
+        'mba_tribune', 
+        'mba_tarps', 
+        'mba_backstage', 
+        'mba_fashion', 
+        'mba_jumbotron'
+    },
+    ["FOOTBALL"] = {
+        'mba_tribune', 
+        'mba_chairs', 
+        'mba_field', 
+        'mba_soccer'
+    },
+    ["ICEHOCKEY"] = {
+        'mba_tribune', 
+        'mba_chairs', 
+        'mba_field', 
+        'mba_hockey'
+    },
+    ["GOKARTA"] = {
+        'mba_cover', 
+        'mba_gokart_01'
+    },
+    ["GOKARTB"] = {
+        'mba_cover', 
+        'mba_gokart_02'
+    },
+    ["TRACKMANIAA"] = {
+        'mba_trackmania_01', 
+        'mba_cover'
+    },
+    ["TRACKMANIAB"] = {
+        'mba_trackmania_02', 
+        'mba_cover'
+    },
+    ["TRACKMANIAC"] = {
+        'mba_trackmania_03', 
+        'mba_cover'
+    },
+    ["TRACKMANIAD"] = {
+        'mba_trackmania_04', 
+        'mba_cover'
+    },
+    ["MMA"] = {
+        'mba_tribune', 
+        'mba_tarps', 
+        'mba_fighting', 
+        'mba_mma', 
+        'mba_jumbotron'
+    },
+    ["EMPTY"] = {
+        'mba_tribune', 
+        'mba_tarps', 
+        'mba_jumbotron'
+    },
+    ["PAINTBALL"] = {
+        'mba_tribune', 
+        'mba_chairs', 
+        'mba_paintball', 
+        'mba_jumbotron'
+    },
+    ["ROCKETLEAGUE"] = {
+        'mba_tribune', 
+        'mba_chairs', 
+        'mba_rocketleague'
+    },
+    ["WRESTLING"] = {
+        'mba_tribune', 
+        'mba_tarps', 
+        'mba_fighting', 
+        'mba_wrestling', 
+        'mba_jumbotron'
+    },
+    ["RESET"] = {
+        'mba_trackmania_04', 
+        'mba_trackmania_03', 
+        'mba_trackmania_02', 
+        'mba_trackmania_01', 
+        'mba_gokart_02', 
+        'mba_gokart_01', 
+        'mba_hockey', 
+        'mba_field', 
+        'mba_soccer', 
+        'mba_rocketleague', 
+        'mba_curling', 
+        'mba_tribune', 
+        'mba_cover', 
+        'mba_tarps', 
+        'mba_chairs', 
+        'mba_basketball', 
+        'mba_derby', 
+        'mba_paintball', 
+        'mba_fighting', 
+        'mba_wrestling', 
+        'mba_mma', 
+        'mba_boxing', 
+        'mba_backstage', 
+        'mba_concert', 
+        'mba_fashion', 
+        'mba_fameorshame', 
+        'mba_ring_of_fire', 
+        'mba_jumbotron', 
+        'mba_terrain'
+    }
+}
