70b8b5ba3842fdea7c9a67478c94101035be0f6b
xMaddMackx <82854182+xMaddMackx@users.noreply.github.com>
2023-04-02 20:06:25 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,6 +8,11 @@ description "Gabz MBA EntitySet Changer"
 version "1.0"
 
 client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
     'shared.lua',
     'client/*.lua'
 }
